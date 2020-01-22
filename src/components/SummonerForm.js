65ec3c3e4ecf4ebe6afa7a9abe470e7933d2ee49
asantos649 <asantos@alumni.nd.edu>
2020-01-22 11:28:10 -0500

    styled form

--- src/components/SummonerForm.js
+++ src/components/SummonerForm.js
@@ -16,13 +16,16 @@ class SummonerForm extends React.Component {
     submitHandler = (e) => {
         e.preventDefault()
         this.props.submitHandler(this.state.summoner)
+        this.setState(
+            {summoner: ''}
+        )
     }
 
     render(){
         return(
-            <form onSubmit={this.submitHandler}>
-                <input type='text' name='summoner' onChange={this.changeHandler} value={this.state.summoner} placeholder='Enter a Summoner Name'/>
-                <input type='submit'/>
+            <form onSubmit={this.submitHandler} className='summonerForm'>
+                <input type='text' name='summoner' className='formInput' onChange={this.changeHandler} value={this.state.summoner} placeholder='Enter a Summoner Name'/>
+                <input type='submit' className='formSubmit'/>
             </form>
         )
     }
